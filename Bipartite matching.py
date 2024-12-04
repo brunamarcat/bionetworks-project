fd638c314bd0400c3a041c5d0f0aac13d17e4ec6
brunamarcat <150627637+brunamarcat@users.noreply.github.com>
2024-12-04 08:23:38 +0100

    3 ways bipartite matching

--- Bipartite matching.py
+++ Bipartite matching.py
@@ -19,15 +19,21 @@ cell_color = sc.get.obs_df(adata, keys='Class_own')
 G = nx.Graph()
 
 # Add nodes with the node attribute "bipartite"
-G.add_nodes_from(list(set((color))), bipartite=0)
-G.add_nodes_from(list(set(cells_type.dropna().values)), bipartite=1)
+G.add_nodes_from(list(set(cells_type.dropna().values)), bipartite=0)
+G.add_nodes_from(list(set((color))), bipartite=1)
 
 for c in set(color):
     idx = np.where(np.array(color) == c)[0]
     for ct in set(cells_type.dropna().values):
-        G.add_edge(c,ct,weight = (cells_type[idx] == ct).sum()/len(color))
+        #G.add_edge(c,ct,weight = (cells_type[idx] == ct).sum()/len(color))
+        a = np.where(cells_type[:]==ct)
+        b=np.where(np.array(color)==c)
+        G.add_edge(c,ct,weight = (cells_type[idx] == ct).sum()/np.union1d(a,b).shape[0]) #Jaccard distance (intersection/union)
+        #G.add_edge(c,ct,weight = (cells_type[idx] == ct).sum()/np.min(a.shape[0],b.shape[0]))
 
 #matching = nx.bipartite.maximum_matching(G)
 matching = nx.algorithms.matching.max_weight_matching(G, maxcardinality=True) 
 print(matching)
+
+
 
